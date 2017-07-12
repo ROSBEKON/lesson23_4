3a526c7bf867a17a394cda375883c7e576018766
dima-test <dima.ul@mail.ru>
2017-07-13 00:51:17 +0400

    finish commit 1

--- lesson23_4.rb
+++ lesson23_4.rb
@@ -1,2 +1,15 @@
-#
-puts "hello"
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative "messages.rb"
+require_relative "answers.rb"
+
+puts message.to_s
+sleep 1
+puts answer.to_s
